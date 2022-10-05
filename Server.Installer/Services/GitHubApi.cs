1c8b1dc38fefb5894f7ca9193968c5e2892c9265
almir1904 <almir1904@users.noreply.github.com>
2022-10-05 14:36:35 +0200

    Update GitHubApi.cs

--- Server.Installer/Services/GitHubApi.cs
+++ Server.Installer/Services/GitHubApi.cs
@@ -1,4 +1,4 @@
-﻿using Remotely.Server.Installer.Models;
+using Remotely.Server.Installer.Models;
 using Remotely.Shared.Utilities;
 using Server.Installer.Models;
 using System;
@@ -113,7 +113,7 @@ namespace Server.Installer.Services
         {
             try
             {
-                var response = await _httpClient.GetFromJsonAsync<GitHubReleasesResponsePayload>("https://api.github.com/repos/lucent-sea/Remotely/releases/latest");
+                var response = await _httpClient.GetFromJsonAsync<GitHubReleasesResponsePayload>("https://api.github.com/repos/immense/Remotely/releases/latest");
                 return response.tag_name;
             }
             catch (Exception ex)
