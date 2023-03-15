a9d6e31a935e1c90d323e6db335c44b27848ad3b
almir1904 <almir1904@users.noreply.github.com>
2023-03-16 00:33:35 +0100

    Update DockerMain.sh

--- Server/DockerMain.sh
+++ Server/DockerMain.sh
@@ -7,6 +7,18 @@ RemotelyData=/remotely-data
 AppSettingsVolume=/remotely-data/appsettings.json
 AppSettingsSrc=/app/appsettings.json
 
+AppSettingsVolume=/remotely-data/Remotely.db
+AppSettingsSrc=/app/Remotely.db
+
+if [ ! -f "$DBSettingsVolume" ]; then
+	echo "Copying RemotelyDB to volume."
+	cp "$DBettingsSrc" "$DBSettingsVolume"
+fi
+
+if [ -f "$DBSettingsSrc" ]; then
+	rm "$DBSettingsSrc"
+fi
+
 if [ ! -f "$AppSettingsVolume" ]; then
 	echo "Copying appsettings.json to volume."
 	cp "$AppSettingsSrc" "$AppSettingsVolume"
@@ -17,6 +29,7 @@ if [ -f "$AppSettingsSrc" ]; then
 fi
 
 ln -s "$AppSettingsVolume" "$AppSettingsSrc"
+ln -s "$DBSettingsVolume" "$DBSettingsSrc"
 
 echo "Starting Remotely server."
 exec /usr/bin/dotnet /app/Remotely_Server.dll
